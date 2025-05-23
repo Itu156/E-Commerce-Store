958bcb576a6f1107c5e9421d00aa93cbd3a46038
Aidan Coetzee <164910914+APC-123@users.noreply.github.com>
2025-05-23 13:59:19 +0200

    commit

--- Javascript/catJS.js
+++ Javascript/catJS.js
@@ -5,45 +5,45 @@ const menMenu = document.getElementById('menMenu');
   const dropdown = menMenu.querySelector('.dropdown');
 
   menMenu.addEventListener('click', (e) => {
-    // Stop bubbling so clicking inside dropdown doesn't re-trigger
+    
     e.stopPropagation();
     dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
   });
 
-  // Hide dropdown if you click anywhere else
+  
   document.addEventListener('click', () => {
     dropdown.style.display = 'none';
   });
   
   womenMenu.addEventListener('click', (e) => {
-    // Stop bubbling so clicking inside dropdown doesn't re-trigger
+    
     e.stopPropagation();
     dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
   });
 
-  // Hide dropdown if you click anywhere else
+  
   document.addEventListener('click', () => {
     dropdown.style.display = 'none';
   });
   
    kid.addEventListener('click', (e) => {
-    // Stop bubbling so clicking inside dropdown doesn't re-trigger
+    
     e.stopPropagation();
     dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
   });
 
-  // Hide dropdown if you click anywhere else
+  
   document.addEventListener('click', () => {
     dropdown.style.display = 'none';
   });
   
    sale.addEventListener('click', (e) => {
-    // Stop bubbling so clicking inside dropdown doesn't re-trigger
+    
     e.stopPropagation();
     dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
   });
 
-  // Hide dropdown if you click anywhere else
+  
   document.addEventListener('click', () => {
     dropdown.style.display = 'none';
   });
